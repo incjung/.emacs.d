93af6d2c650e996619e372526d12a1b944bf6538
incjung <incjjung@gmail.com>
2022-10-03 10:33:29 +1300

    Create init.el

---
+++ init.el
@@ -0,0 +1,18 @@
+(require 'org)
+
+(org-babel-load-file
+ (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(easy-hugo ivy-posframe yasnippet-snippets yasnippet swiper-helm counsel ivy magit yaml-mode which-key)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
